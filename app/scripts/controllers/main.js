0b26b5550ae989d2a671d8d2409e98ecf361db6d
alegalviz <alegalviz@gmail.com>
2017-04-20 13:34:56 -0300

    conexión con lineas curvas

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,11 @@
  * # MainCtrl
  * Controller of the vtimelineApp
  */
-var vtimelineApp = angular.module('vtimelineApp');
+var vtimelineApp = angular.module('vtimelineApp')
+  .constant('connConfig', {
+    method: {},
+    event: {}
+  });
 
 vtimelineApp.controller('MainCtrl', function ($scope, hitosRepository) {
 
@@ -33,7 +37,7 @@ vtimelineApp.factory('hitosRepository', function($http) {
   return {
     getAllHitos: function() {
 
-        var url = 'json/puhal-v2.json';
+        var url = 'json/test.json';
         return $http.get(url);
 
     }
@@ -62,7 +66,6 @@ vtimelineApp.filter('emptyFilter', function() {
         var filteredArray = [];
         angular.forEach(array, function(item) {
             if (item.titulo) {
-
                 filteredArray.push(item);
             }
         });
@@ -70,16 +73,84 @@ vtimelineApp.filter('emptyFilter', function() {
     };
 });
 
-vtimelineApp.directive('lineConnection', function(){
+vtimelineApp.directive('conecciones', [
+  '$timeout', 'connConfig', function ($timeout, connConfig) {
+    return {
+      scope: {
+        options: '=',
+        enabled: '@'
+      },
+      restrict: 'AE',
+      link: function (scope, element) {
+        //hide slider
+        //angular.element(element).css('display', 'none');
+
+        var options, initOptions, destroy, init, destroyAndInit;
+
+        initOptions = function () {
+          options = angular.extend(angular.copy(connConfig),{
+            enabled: scope.enabled !== 'false'
+          }, scope.options);
+
+        };
+
+        destroy = function () {};
+
+        init = function () {
+          initOptions();
+
+          var aconectar = angular.element(element);
+
+          if (angular.element(element).hasClass('conn-initialized')) {
+            if (options.enabled) {
+              //return slickness.slick('getSlick');
+            } else {
+              destroy();
+            }
+          } else {
+            if (!options.enabled) {
+              return;
+            }
+            $timeout(function () {
+              //angular.element(element).css('display', 'block');
+              aconectar.find('.circulo').connections({all:false});
+            });
+          }
+        };
+
+        destroyAndInit = function () {
+          destroy();
+          init();
+        };
+
+        element.one('$destroy', function () {
+          destroy();
+        });
+
+        return scope.$watch('options', function (newVal) {
+          if (newVal !== null) {
+            return destroyAndInit();
+          }
+        }, true);
+      }
+    };
+  }
+]);
+
+/*vtimelineApp.directive('conectarLineas', function() {
   return {
     restrict: 'A',
-    link: function(scope, el){
-      $(el).connections();
+    link: function( $scope ) {
+      $scope.$on('repeat_done', function( domainElement ) {
+        domainElement.find('.circulo').connections();
+      } );
     }
-  }
-});
-$(document).ready(function() {
+  };
+});*/
+
+
+/*$(document).ready(function() {
   $('.circulos').connections();
   var connections = $('connection, inner');
   setInterval(function() { connections.connections('update') }, 100);
-});
+});*/
