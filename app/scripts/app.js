666407f8ca8e99e43238fab9df8f72578883322b
alegalviz <alegalviz@gmail.com>
2017-05-16 14:18:09 -0300

    nuevo disenio

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,3 +33,28 @@ angular
         redirectTo: '/'
       });
   });
+
+$(function(){
+  $('#maximage').maximage({
+    cycleOptions: {
+      fx: 'fade',
+      // Speed has to match the speed for CSS transitions
+      speed: 1000,
+      timeout: 0,
+      prev: '#arrow_left',
+      next: '#arrow_right'
+    },
+    onFirstImageLoaded: function(){
+      $('#cycle-loader').hide();
+      $('#maximage').fadeIn('fast');
+    },
+    // cssBackgroundSize might be causing choppiness in retina display safari
+    cssBackgroundSize: true
+  });
+
+  // Helper function to Fill and Center the HTML5 Video
+  $('#html5video').maximage('maxcover');
+
+  // To show it is dynamic html text
+  $('.in-slide-content').delay(1200).fadeIn();
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,7 +14,7 @@ var vtimelineApp = angular.module('vtimelineApp')
     event: {}
   });
 
-vtimelineApp.controller('MainCtrl', function ($scope, hitosRepository) {
+vtimelineApp.controller('MainCtrl', function ($scope, hitosRepository, anchorDesplazarSuave) {
 
   hitosRepository.getAllHitos().then(function(hitos) {
      $scope.hitos = hitos.data.filter(function (i){
@@ -29,6 +29,11 @@ vtimelineApp.controller('MainCtrl', function ($scope, hitosRepository) {
   };
   $scope.removeHito = function (index) {
     $scope.hitos.splice(index, 1);
+  };
+  $scope.continuar = function (eID){
+    // call $anchorScroll()
+    anchorDesplazarSuave.scrollTo(eID);
+
   };
 });
 
@@ -53,7 +58,8 @@ vtimelineApp.filter('fechaFormato', function(moment) {
       months: 'enero febrero marzo abril mayo junio julio agosto septiembre octubre noviembre diciembre'.split(' '),
     });
       var momentObj = moment(input, 'DD \d\e MMMM');
-    var momentString = momentObj.format('DD-MM'); // 2016-07-15
+    var momentString = momentObj.format('DD'); // 2016-07-15
+    momentString += ' de ' + momentObj.format('MMMM').toUpperCase();
 
     return momentString;
 
@@ -89,6 +95,62 @@ vtimelineApp.directive('conecciones', ['$timeout', function($timeout) {
     });
   };
 }]);
+
+vtimelineApp.service('anchorDesplazarSuave', function(){
+
+  this.scrollTo = function(eID) {
+
+    // This scrolling function
+    // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+      scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) {speed = 20;}
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+      for ( var i=startY; i<stopY; i+=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+      } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+      setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+      leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+
+    function currentYPosition() {
+      // Firefox, Chrome, Opera, Safari
+      if (self.pageYOffset) return self.pageYOffset;
+      // Internet Explorer 6 - standards mode
+      if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+      // Internet Explorer 6, 7 and 8
+      if (document.body.scrollTop) return document.body.scrollTop;
+      return 0;
+    }
+
+    function elmYPosition(eID) {
+      var elm = document.getElementById(eID);
+      var y = elm.offsetTop;
+      var node = elm;
+      while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+      } return y;
+    }
+
+  };
+
+});
+
+
 // vtimelineApp.directive('conecciones', [
   // '$timeout', 'connConfig', function ($timeout, connConfig) {
     // return {
@@ -115,7 +177,7 @@ vtimelineApp.directive('conecciones', ['$timeout', function($timeout) {
         // init = function () {
           // initOptions();
 
-          
+
 
           // if (angular.element(element).hasClass('conn-initialized')) {
             // if (options.enabled) {
@@ -161,11 +223,4 @@ vtimelineApp.directive('conecciones', ['$timeout', function($timeout) {
       } );
     }
   };
-});*/
-
-
-/*$(document).ready(function() {
-  $('.circulos').connections();
-  var connections = $('connection, inner');
-  setInterval(function() { connections.connections('update') }, 100);
 });*/
